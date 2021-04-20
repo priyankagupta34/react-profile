13e0885801b70de132b025f44502249120e291da
Gupta Priyanka (RBEI/ERD1) <Priyanka.Gupta@in.bosch.com>
2021-04-20 21:23:56 +0530

    Minor css changes

--- src/js/menu-buttons/MenuButtons.js
+++ src/js/menu-buttons/MenuButtons.js
@@ -6,7 +6,7 @@ export default class MenuButtons extends Component {
     static contextType = ThemeContext;
     render() {
         const { options } = this.props;
-        const { btnTheme, backgroundColor, boxShadow } = Theme[this.context];
+        const { btnTheme, backgroundColor, boxShadow, sectionBackground } = Theme[this.context];
         return (
             <nav className="descNav" style={{ backgroundColor }}>
                 <div></div>
@@ -15,9 +15,9 @@ export default class MenuButtons extends Component {
                 <i className={options ? "fa fa-angle-up doE" : "fa fa-angle-up upE"}></i>
                 </button>
                 {options && <div className="optionPage" style={{ boxShadow }}>
-                    <div className="option" onClick={() => this.props.changeThemeHandler_('blueTheme')}>blue</div>
-                    <div className="option" onClick={() => this.props.changeThemeHandler_('magentaTheme')}>magenta</div>
-                    <div className="option" onClick={() => this.props.changeThemeHandler_('yellowTheme')}>yellow</div>
+                    <div className="option" style={{backgroundColor: sectionBackground}} onClick={() => this.props.changeThemeHandler_('blueTheme')}>blue</div>
+                    <div className="option" style={{backgroundColor: sectionBackground}} onClick={() => this.props.changeThemeHandler_('magentaTheme')}>magenta</div>
+                    <div className="option" style={{backgroundColor: sectionBackground}} onClick={() => this.props.changeThemeHandler_('yellowTheme')}>yellow</div>
                 </div>}
             </nav>
         )
