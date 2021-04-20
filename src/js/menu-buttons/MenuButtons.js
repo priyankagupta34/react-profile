7d002bdadcd939860333281d937bd01c8dc763b5
Gupta Priyanka (RBEI/ERD1) <Priyanka.Gupta@in.bosch.com>
2021-04-20 21:33:30 +0530

    Minor css changes

--- src/js/menu-buttons/MenuButtons.js
+++ src/js/menu-buttons/MenuButtons.js
@@ -12,9 +12,9 @@ export default class MenuButtons extends Component {
                 <div></div>
                 <button className={`menuBtn ${btnTheme}`}>About</button>
                 <button className={`menuBtn ${btnTheme}`} onClick={this.props.openOptions}>Theme
-                <i className={options ? "fa fa-angle-up doE" : "fa fa-angle-up upE"}></i>
+                <i className={options ? "fa fa-angle-up doE" : "fa fa-angle-up upE"} ></i>
                 </button>
-                {options && <div className="optionPage" style={{ boxShadow }}>
+                {options && <div className="optionPage" style={{ boxShadow}}>
                     <div className="option" style={{backgroundColor: sectionBackground}} onClick={() => this.props.changeThemeHandler_('blueTheme')}>blue</div>
                     <div className="option" style={{backgroundColor: sectionBackground}} onClick={() => this.props.changeThemeHandler_('magentaTheme')}>magenta</div>
                     <div className="option" style={{backgroundColor: sectionBackground}} onClick={() => this.props.changeThemeHandler_('yellowTheme')}>yellow</div>
